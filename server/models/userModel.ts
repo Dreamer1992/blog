d7fb23f0532987b0db4e6e28aca5ece61f06adfb
Davlet Ganiev <Ganiev.Davlet@gmail.com>
2021-10-25 23:47:54 +0300

    project init

---
+++ server/models/userModel.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Укажите Ваше имя"],
+        trim: true,
+        maxLength: [20, "Имя должно быть не больше 20-ти символов"]
+    },
+    account: {
+        type: String,
+        required: [true, "Укажите почту или номер телефона"],
+        trim: true,
+        unique: true
+    },
+    avatar: {
+        type: String,
+        default: 'https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Free-Download.png'
+    },
+    type: {
+        type: String,
+        default: 'normal'
+    }
+}, {
+    timestamps: true
+})
+
+export default mongoose.model('User', userSchema)
